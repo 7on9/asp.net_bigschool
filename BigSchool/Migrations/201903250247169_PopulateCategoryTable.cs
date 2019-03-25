f0ab4b2c5f1ec402d4dfc50a831c1708dca12b33
sinhvien <sinhvien>
2019-03-25 11:12:36 +0700

    can login and create course

---
+++ BigSchool/Migrations/201903250247169_PopulateCategoryTable.cs
@@ -0,0 +1,18 @@
+﻿namespace BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateCategoryTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES(1, N'INVESTIGATE LIKE SHERLOCK HOLMES')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES(2, N'CODE WITH NGUYỄN ĐÌNH ÁNH')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES(3, N'HỌC LÀM GIÀU')");
+        }  
+        public override void Down()
+        {
+        }
+    }
+}
